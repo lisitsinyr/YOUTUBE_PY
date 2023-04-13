db4cb7e91691e4d98711b4dd9808fefc72d02ec4
lisitsinyr <lisitsinyr@gmail.com>
2023-04-13 10:32:45 +0400

    13.04.2023

--- YOUTUBE_FormMainWindow.py
+++ YOUTUBE_FormMainWindow.py
@@ -139,6 +139,9 @@ class FormMainWindow(QMainWindow):
 
         self.__SetStatApplication (LUProc.TStatApplication.caBreak)
 
+
+
+
         # Splash = TSplash.Create (Self)
         # Splash.Name = 'Splash'
         # Splash.Height = StatusBar_P1_P2.Height
